c0aecea37a176be780a4ea49d420e4f2f37c9987
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-06-05 18:49:10 +0600

    adding Header().Set(Content-Type)

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
-package main
+package main // project should start from here!
 
 import (
 	"fmt"
-	"net/http"
+	"net/http" // these are packages ! creating web server , handling web request and so on!
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my Tutorial</h1>")
+	w.Header().Set("Content-Type", "text/html") // text/plain
+	fmt.Fprint(w, "<h1>Welcome to___ my super Awesome Tutorial</h1>")
 }
 
 func main() {
@@ -15,4 +16,3 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 
 }
-
